49180a5b0ba924c22c20e2cbe4f632b81acc0ac0
Puyed <amrut-rajesh.puyed@capgemini.com>
2023-10-25 14:42:10 +0530

    Untrack node_modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const path = require("path");
 const hbs = require("hbs");
+const jwt = require("jsonwebtoken")
 
 const templatePath = path.join(__dirname, "../templates");
 const collection = require("./mongodb")
@@ -22,57 +24,68 @@ app.get("/signup", (req, res) => {
     res.render("signup");
 });
 
+
+// Signup
 app.post("/signup", async (req, res) => {
-    const data = {
+    try {
+      // Create an instance of the collection model
+      const data = new collection({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-    };
-
-    try {
-
-        console.log("Data to be inserted:", data);
-        const user = await collection.create(data);
-        if (user) {
-            console.log("Data inserted successfully:", user);
-            res.render("home");
-        } else {
-            console.error("Data insertion failed");
-            res.status(500).send("Signup failed");
-        }
-    } catch (err) {
-        console.error("Error:", err);
+      });
+  
+      console.log("Data to be inserted:", data);
+  
+      // Use save() to save the instance to the database
+      const user = await data.save();
+  
+      if (user) {
+        // Call the generateAuthToken method on the model itself
+        const token = await data.generateAuthToken();
+        console.log("Generated token for signup: "  + token);
+        // console.log("Data inserted successfully:", user);
+        // console.log(token);
+        res.render("home");
+      } else {
+        console.error("Data insertion failed");
         res.status(500).send("Signup failed");
+      }
+    } catch (err) {
+      console.error("Error:", err);
+      res.status(500).send("Signup failed");
     }
-});
+  });
+  
 
 // for Login
+// for Login
 app.post("/login", async (req, res) => {
-
-
     try {
-        const check = await collection.findOne({ name: req.body.name })
-
-        if (check.password === req.body.password) {
-            res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` })
-        }
-
-        else {
-            res.send("incorrect password")
-        }
-
-
+      console.log("Login attempt with username: " + req.body.name);
+      console.log("Password: " + req.body.password);
+  
+      const user = await collection.findOne({ name: req.body.name, password: req.body.password });
+  
+      if (user) {
+        // User found with matching credentials
+        console.log("Login successful for username: " + req.body.name);
+        const token = await user.generateAuthToken(); // Call the method on the user instance
+        console.log("Generated token: for login " + token);
+        res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}`, token });
+      } else {
+        // User not found or incorrect password
+        console.log("Login failed for username: " + req.body.name);
+        res.send("Incorrect username or password");
+      }
+    } catch (e) {
+      console.error("Error:", e);
+      res.status(500).send("An error occurred during login");
     }
-
-    catch (e) {
-
-        res.send("wrong details")
-
-
-    }
-
-
-});
+  });
+  
+  
+  
 
 
 app.listen(3000, () => {
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,4 +1,6 @@
+
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 mongoose.connect("mongodb://localhost:27017/LoginSignup", {
   useNewUrlParser: true,
@@ -24,7 +26,29 @@ const LoginSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
+  tokens:[{
+    token:{
+      type:String,
+      required:true,
+    }
+  }]
 });
+
+// Define the generateAuthToken method within the schema
+LoginSchema.methods.generateAuthToken = async function () {
+  try {
+    const token = jwt.sign({ _id: this._id.toString() }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({token:token})
+
+    // save to database
+    await this.save();
+    return token; // Return the generated token
+  } catch (err) {
+    console.log("Error generating token:", err);
+    throw err;
+  }
+}
 
 const collection = mongoose.model("collection1", LoginSchema);
 
